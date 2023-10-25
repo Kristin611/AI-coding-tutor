034980a4311051dc610ce7e033ad57c1f2f287ac
Kris10 <kmrichie611@gmail.com>
2023-10-25 14:30:25 -0500

    moved prompt object into try/catch block

--- script.js
+++ script.js
@@ -11,13 +11,25 @@ const model = new OpenAI({
     temperature: 0,
     model: 'gpt-3.5-turbo'
 });
-console.log({model})
+//console.log({model})
 
 //uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
 const promptFunc = async (input) => { //using input parameter to capture user question. then will need to create an init function that when called will execute an inquirer prompt that will ask the user for a question about a coding concept. 
     try {
         const res = await model.call(input);
         console.log(res);
+
+        //instantiation of a new object called "prompt" using the "PromptTemplate" class
+    const prompt = new PromptTemplate({
+        template: "You are a javascript expert and will answer the user's coding questions as thoroughly as possible", //the template allows us to set the stage of our application by giving the AI context about how it should behave and respond to user questions. 
+        inputVariables: ['question'], //allows us to inject user input directly into our template so wtvr question the user asks will always be within the confines of the overall context that the developer sets.
+    });
+
+    //using the .format() method on our instantiated prompt object to pass in user input to our template. The key of the object being passed into the prompt method mathces the variable name, question, and value is the user input captured from inquirer.    
+    const promptInput = await prompt.format({
+        question: input
+     });
+
     }
     catch (err) {
         console.error(err);
@@ -41,13 +53,6 @@ const init = () => {
 //calls the initialization function and starts the script
 init();
 
-//instantiation of a new object called "prompt" using the "PromptTemplate" class
-    const prompt = new PromptTemplate({
-        template: "You are a javascript expert and will answer the user's coding questions as thoroughly as possible", //the template allows us to set the stage of our application by giving the AI context about how it should behave and respond to user questions. 
-        inputVariables: ['question'], //allows us to inject user input directly into our template so wtvr question the user asks will always be within the confines of the overall context that the developer sets.
-    });
 
- //using the .format() method on our instantiated prompt object to pass in user input to our template. The key of the object being passed into the prompt method mathces the variable name, question, and value is the user input captured from inquirer.    
-    const promptInput = await prompt.format({
-       question: input
-    });
+
+ 
